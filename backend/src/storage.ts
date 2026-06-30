07a5ec21a2c39f5ac95ab265df3132521e1344d2
Landon Redmond <landon.redmond0@gmail.com>
2026-06-30 09:55:07 -0500

    Add storage utility functions for file operations

---
+++ backend/src/storage.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+
+const STORAGE_DIRECTORY: string = path.join(".", process.env.STORAGE_DIRECTORY!);
+
+if (!fs.existsSync(STORAGE_DIRECTORY)) fs.mkdirSync(STORAGE_DIRECTORY, { recursive: true });
+
+export async function writeFile(filename: string, data: Buffer): Promise<void> {
+    const filePath = path.join(STORAGE_DIRECTORY, filename);
+    await fs.promises.writeFile(filePath, data);
+}
+
+export async function readFile(filename: string): Promise<Buffer> {
+    const filePath = path.join(STORAGE_DIRECTORY, filename);
+    return await fs.promises.readFile(filePath);
+}
+
+export async function readDirectory(directory: string): Promise<string[]> {
+    const directoryPath = path.join(STORAGE_DIRECTORY, directory);
+    return await fs.promises.readdir(directoryPath);
+}
