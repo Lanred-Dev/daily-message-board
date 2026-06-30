11a7a4d24eb3c8baf611814fa6163334d98b39b9
Landon Redmond <landon.redmond0@gmail.com>
2026-06-30 09:55:26 -0500

    Refactor index.ts to register all route handlers for messages, images, calendar, and time

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,24 @@
+import multipart from "@fastify/multipart";
 import Fastify from "fastify";
+import { getCalendarRoute } from "./routes/calendar";
+import { getImageRoute, postImageRoute } from "./routes/image";
 import indexRoute from "./routes/index";
+import { getMessageRoute, postMessageRoute } from "./routes/message";
+import sendDiscordMessageRoute from "./routes/send-discord-message";
+import { getUpdateTimeRoute } from "./routes/time";
 
 const app = Fastify();
 
+app.register(multipart);
+
 app.route(indexRoute);
+app.route(getMessageRoute);
+app.route(postMessageRoute);
+app.route(getUpdateTimeRoute);
+app.route(getImageRoute);
+app.route(postImageRoute);
+app.route(getCalendarRoute);
+app.route(sendDiscordMessageRoute);
 
 app.listen({ port: parseInt(process.env.PORT!), host: "0.0.0.0" }, function (error) {
     if (error) {
