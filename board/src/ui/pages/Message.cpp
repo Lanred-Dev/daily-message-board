e95951237537ab90ee318779d9f119d6408b6dd1
Landon Redmond <landon.redmond0@gmail.com>
2026-07-01 09:47:56 -0500

    Refactor MessagePage setup to use Color struct for text color

--- board/src/ui/pages/Message.cpp
+++ board/src/ui/pages/Message.cpp
@@ -2,9 +2,10 @@
 #include "ui/components/TextLabel.h"
 #include "systems/Message.h"
 #include "config.h"
+#include "Color.h"
 
 void MessagePage::setup()
 {
-    TextLabel *messageLabel = new TextLabel(Vector2(10, 10), Vector2(DISPLAY_WIDTH - 20, DISPLAY_HEIGHT - 20), std::get<0>(MessageSystem::getCurrentMessage()), Display::Color::WHITE, 1.2f);
+    TextLabel *messageLabel = new TextLabel(Vector2(10, 10), Vector2(DISPLAY_WIDTH - 20, DISPLAY_HEIGHT - 20), 1, std::get<0>(MessageSystem::getCurrentMessage()), Color::TEXT_MAIN, 1.2f);
     addInstance(messageLabel);
 }
