d14f12ff3fec0449749818ded76a1f4f58f6e3ff
Landon Redmond <landon.redmond0@gmail.com>
2026-07-01 17:19:23 -0500

    Update CircleButton color in Navbar setup to use LIGHT_BUTTON

--- board/src/ui/Navbar.cpp
+++ board/src/ui/Navbar.cpp
@@ -21,15 +21,15 @@ void Navbar::setup()
     const int startX = NAVBAR_POSITION.x + (NAVBAR_WIDTH / 2) - (totalRowWidth / 2) + buttonRadius;
     const int centerY = NAVBAR_POSITION.y + (NAVBAR_HEIGHT / 2);
 
-    CircleButton *messagePageButton = new CircleButton(Vector2(startX, centerY), buttonRadius, 10, Color::PRIMARY_BUTTON);
+    CircleButton *messagePageButton = new CircleButton(Vector2(startX, centerY), buttonRadius, 10, Color::LIGHT_BUTTON);
     messagePageButton->onPress.connect([]() { UI::setCurrentPage(0); });
     UI::addInstance(messagePageButton);
 
-    CircleButton *imagePageButton = new CircleButton(Vector2(startX + buttonDiameter + buttonSpacing, centerY), buttonRadius, 10, Color::PRIMARY_BUTTON);
+    CircleButton *imagePageButton = new CircleButton(Vector2(startX + buttonDiameter + buttonSpacing, centerY), buttonRadius, 10, Color::LIGHT_BUTTON);
     imagePageButton->onPress.connect([]() { UI::setCurrentPage(1); });
     UI::addInstance(imagePageButton);
 
-    CircleButton *calendarPageButton = new CircleButton(Vector2(startX + 2 * (buttonDiameter + buttonSpacing), centerY), buttonRadius, 10, Color::PRIMARY_BUTTON);
+    CircleButton *calendarPageButton = new CircleButton(Vector2(startX + 2 * (buttonDiameter + buttonSpacing), centerY), buttonRadius, 10, Color::LIGHT_BUTTON);
     calendarPageButton->onPress.connect([]() { UI::setCurrentPage(2); });
     UI::addInstance(calendarPageButton);
 }
