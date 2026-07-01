3c68cd885b28736024cb11a61aceee5123dd25f4
Landon Redmond <landon.redmond0@gmail.com>
2026-07-01 09:48:14 -0500

    Refactor Navbar setup to use CircleButton components and improve layout calculations

--- board/src/ui/Navbar.cpp
+++ board/src/ui/Navbar.cpp
@@ -1,15 +1,38 @@
 #include "Navbar.h"
 #include "ui/components/Container.h"
-#include "ui/components/Button.h"
+#include "ui/components/CircleButton.h"
 #include "ui/components/TextLabel.h"
 #include "systems/UI.h"
 #include "config.h"
+#include "Color.h"
 
-const int NAVBAR_HEIGHT = 40;
+const int NAVBAR_HEIGHT = 30;
 const int NAVBAR_WIDTH = DISPLAY_WIDTH / 2;
+const Vector2 NAVBAR_POSITION(DISPLAY_WIDTH / 2 - NAVBAR_WIDTH / 2, DISPLAY_HEIGHT - NAVBAR_HEIGHT - 10);
 
 void Navbar::setup()
 {
-    Container *background = new Container(Vector2(DISPLAY_WIDTH / 2 - NAVBAR_WIDTH / 2, 10), Vector2(NAVBAR_WIDTH, NAVBAR_HEIGHT), Display::Color::CONTAINER_BACKGROUND, 8);
+    Container *background = new Container(NAVBAR_POSITION, Vector2(NAVBAR_WIDTH, NAVBAR_HEIGHT), 99, Color::CONTAINER_BACKGROUND, 15);
     UI::addInstance(background);
+
+    const int buttonRadius = 15;
+    const int buttonDiameter = buttonRadius * 2;
+    const int buttonSpacing = 6;
+    const int totalButtons = 3;
+
+    const int totalRowWidth = (totalButtons * buttonDiameter) + ((totalButtons - 1) * buttonSpacing);
+    const int startX = NAVBAR_POSITION.x + (NAVBAR_WIDTH / 2) - (totalRowWidth / 2) + buttonRadius;
+    const int centerY = NAVBAR_POSITION.y + (NAVBAR_HEIGHT / 2);
+
+    CircleButton *messagePageButton = new CircleButton(Vector2(startX, centerY), buttonRadius, 10, Color::PRIMARY_BUTTON);
+    messagePageButton->onPress.connect([]() { UI::setCurrentPage(0); });
+    UI::addInstance(messagePageButton);
+
+    CircleButton *imagePageButton = new CircleButton(Vector2(startX + buttonDiameter + buttonSpacing, centerY), buttonRadius, 10, Color::PRIMARY_BUTTON);
+    imagePageButton->onPress.connect([]() { UI::setCurrentPage(1); });
+    UI::addInstance(imagePageButton);
+
+    CircleButton *calendarPageButton = new CircleButton(Vector2(startX + 2 * (buttonDiameter + buttonSpacing), centerY), buttonRadius, 10, Color::PRIMARY_BUTTON);
+    calendarPageButton->onPress.connect([]() { UI::setCurrentPage(2); });
+    UI::addInstance(calendarPageButton);
 }
