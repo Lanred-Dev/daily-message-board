b29d52498f6889cb7a45661491e304dc4b640233
Landon Redmond <landon.redmond0@gmail.com>
2026-07-01 00:35:32 -0500

    Refactor Display class to improve touch handling and display setup

--- board/src/core/Display.cpp
+++ board/src/core/Display.cpp
@@ -1,10 +1,16 @@
 #include "Display.h"
+#include "config.h"
+#include "systems/UI.h"
 
 BootStatus Display::setup()
 {
     lcd.init();
     lcd.setRotation(1);
-    
+
+    canvas.setColorDepth(8);
+
+    if (canvas.createSprite(DISPLAY_WIDTH, DISPLAY_HEIGHT) == nullptr) return {"Failed to create display canvas sprite.", false};
+
     clear();
 
     return {"Display initialized successfully.", true};
@@ -12,5 +18,34 @@ BootStatus Display::setup()
 
 void Display::clear(const uint16_t color)
 {
-    lcd.fillScreen(color);
+    canvas.fillScreen(color);
+}
+
+void Display::loop()
+{
+    Display::updateTouch();
+    UI::draw();
+    canvas.pushSprite(0, 0);
+    delay(16);
+}
+
+void Display::updateTouch() {
+    int16_t touchX = 0;
+    int16_t touchY = 0;
+
+    if (lcd.getTouch(&touchX, &touchY)) {
+        touchPosition.x = touchX;
+        touchPosition.y = touchY;
+        isTouched = true;
+    } else {
+        isTouched = false;
+    }
+}
+
+std::optional<Vector2> Display::getTouchPosition() {
+    if (!isTouched) {
+        return std::nullopt;
+    }
+
+    return touchPosition;
 }
--- board/src/core/Display.h
+++ board/src/core/Display.h
@@ -1,61 +1,21 @@
 #pragma once
 
 #include "classes/BootStatus.h"
-#include <LovyanGFX.hpp>
-
-class LGFX_E32R28T : public lgfx::LGFX_Device
-{
-    lgfx::Panel_ILI9341 _panel_instance;
-    lgfx::Bus_SPI _bus_instance;
-    lgfx::Light_PWM _light_instance;
-
-public:
-    LGFX_E32R28T(void)
-    {
-        {
-            auto cfg = _bus_instance.config();
-            cfg.spi_host = VSPI_HOST;
-            cfg.spi_mode = 0;
-            cfg.freq_write = 27000000;
-            cfg.freq_read = 16000000;
-            cfg.pin_sclk = 14;
-            cfg.pin_mosi = 13;
-            cfg.pin_miso = 12;
-            cfg.pin_dc = 2;
-            _bus_instance.config(cfg);
-            _panel_instance.setBus(&_bus_instance);
-        }
-        {
-            auto cfg = _panel_instance.config();
-            cfg.pin_cs = 15;
-            cfg.pin_rst = -1;
-            cfg.memory_width = 240;
-            cfg.memory_height = 320;
-            cfg.panel_width = 240;
-            cfg.panel_height = 320;
-            cfg.offset_x = 0;
-            cfg.offset_y = 0;
-            _panel_instance.config(cfg);
-        }
-        {
-            auto cfg = _light_instance.config();
-            cfg.pin_bl = 21;
-            cfg.freq = 44100;
-            cfg.pwm_channel = 7;
-            _light_instance.config(cfg);
-            _panel_instance.setLight(&_light_instance);
-        }
-        setPanel(&_panel_instance);
-    }
-};
+#include "classes/LGFX_E32R28T.h"
+#include "math/Vector2.h"
+#include <optional>
 
 namespace Display
 {
-    inline static LGFX_E32R28T lcd;
+    inline LGFX_E32R28T lcd;
+    inline LGFX_Sprite canvas(&lcd);
+    inline Vector2 touchPosition;
+    inline bool isTouched = false;
 
     struct Color
     {
         static constexpr uint16_t BLACK = 0x0000;
+        static constexpr uint16_t CONTAINER_BACKGROUND = 0x18C3;
         static constexpr uint16_t WHITE = 0xFFFF;
         static constexpr uint16_t RED = 0xF800;
         static constexpr uint16_t GREEN = 0x07E0;
@@ -66,4 +26,7 @@ namespace Display
 
     BootStatus setup();
     void clear(const uint16_t color = Color::BLACK);
+    void loop();
+    void updateTouch();
+    std::optional<Vector2> getTouchPosition();
 }
